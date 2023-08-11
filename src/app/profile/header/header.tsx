496d96f139d33b21aecfaaeb0d7404b46e4286f4
Rishabh Yadav <github.for.rishabh@gmail.com>
2023-08-11 21:39:45 +0530

    new profile contain added

---
+++ src/app/profile/header/header.tsx
@@ -0,0 +1,18 @@
+export default function header(){
+    return(
+        <div>
+            
+        <div className="">
+        <img src="img/cloud.png" alt="top-cloud" className=""/>
+        <h1>
+            Hey! I am Rishabh.
+        </h1>
+        <p>Frontend Web Developer And Java Programmer.
+        </p>
+        <img src="/cloud.png" alt="bottom-cloud" className="bottom-cloud"/>
+        </div>
+    
+        <img src="/mountian.jpg" className="mount" alt="Mountain_Image"/>
+        </div>
+    );
+}
